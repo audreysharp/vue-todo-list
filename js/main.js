43b3954af99369bd48d646b24e160c3b087980ba
audreysharp <audreysharp21@gmail.com>
2017-01-26 18:56:35 -0500

    Initial commit. Add todo working.

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// hint -- remove a particular element from an array: http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+// hint -- add an item to an array: http://stackoverflow.com/questions/351409/how-to-append-something-to-an-array
+// hint -- you may need an "editingItem" variable in your data object
+
+new Vue({
+  el: '#app',
+  data: {
+    titleInput: '',
+    contentInput: '',
+    editingItem: false,
+    todos: [ 
+      {
+        title: 'test',
+        content: 'test2'
+      },
+      {
+        title: 'test2',
+        content: 'test3'
+      }
+    ]
+  },
+  methods: {
+    addTodo: function() {
+      this.todos.push({
+        title: this.titleInput,
+        content: this.contentInput
+      })
+      this.titleInput = '';
+      this.contentInput = '';
+      this.editingItem = false;
+    },
+    removeTodo: function(todo) {
+
+    },
+    isEditing: function() {
+      this.editingItem = true;
+    },
+    cancel: function() {
+      this.titleInput = '';
+      this.contentInput = '';
+      this.editingItem = false;
+    }
+
+  }
+});
